328a05f3bcd9cc6074044e98815c2f478967b590
younesm@hpy <younesmalek2014@gmail.com>
2021-05-16 11:00:12 +0200

    fetch all posts

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,18 @@ from django.shortcuts import render
 from .models import Post
 
 def index(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     context = {
         'posts': latest_posts
     }
     return render(request, 'blog/index.html', context)
 
 def posts(request):
-    return render(request, 'blog/posts.html')
+    all_posts = Post.objects.all().order_by('-date')
+    context = {
+        'posts': all_posts
+    }
+    return render(request, 'blog/posts.html', context)
 
 def post_detail(request, slug):
     return render(request, 'blog/post-detail.html')
