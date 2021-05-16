09c2fdd64bef764ff202e1302c8ce5309e3c82c8
younesm@hpy <younesmalek2014@gmail.com>
2021-05-16 02:25:23 +0200

    Added site models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'author', 'date', )
+    list_display_links = ('id', 'title',)
+    list_filter = ('slug', 'author',)
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+
+admin.site.register(models.Tag)
+admin.site.register(models.Author)
+admin.site.register(models.Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+
+class Tag(models.Model):
+    capture = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.capture
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+
+    def full_name(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self) -> str:
+        return self.full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_url = models.ImageField(upload_to='images/', null=True)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[validators.MinLengthValidator(10)])
+    tags = models.ManyToManyField(Tag)
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name='posts')
+
+    def __str__(self) -> str:
+        return self.title
