0632eff1cc3381a9ec892abd6f4f4186068ec259
younesm@hpy <younesmalek2014@gmail.com>
2021-05-17 02:04:04 +0200

    Added comment model

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,10 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
