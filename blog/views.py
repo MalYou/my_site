8a3d7fdad5d47029f69724e2f324a836d0bc2e53
younesm@hpy <younesmalek2014@gmail.com>
2021-06-08 00:13:57 +0200

    finishing read later feature

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from typing import List
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import ListView, View
 from django.contrib import messages
@@ -26,6 +27,11 @@ class PostsView(ListView):
 
 class PostView(View):
 
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get('stored_posts')
+
+        return True if post_id in stored_posts else False
+
     def get(self, request, slug):
         post = get_object_or_404(Post, slug=slug)
 
@@ -33,7 +39,8 @@ class PostView(View):
             'post': post,
             'tags': post.tags.all(),
             'comments': post.comments.all().order_by('-pk'),
-            'form': CommentForm()
+            'form': CommentForm(),
+            'is_stored_post':  self.is_stored_post(request, post.id)
         }
 
         return render(request, 'blog/post-detail.html', context)
@@ -45,7 +52,8 @@ class PostView(View):
         context = {
             'post': post,
             'tags': post.tags.all(),
-            'comments': post.comments.all().order_by('-pk')
+            'comments': post.comments.all().order_by('-pk'),
+            'is_stored_post':  self.is_stored_post(request, post.id)
         }
 
         if form.is_valid():
@@ -61,3 +69,34 @@ class PostView(View):
 
         messages.error(request, 'Please correct your inputs!')
         return render(request, 'blog/post-detail.html', context)
+
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get('stored_posts')
+        posts = []
+
+        if stored_posts:
+            posts = Post.objects.filter(id__in=stored_posts)
+
+        context = {
+            'posts': posts if posts else None
+        }
+
+        return render(request, 'blog/stored-posts.html', context)
+
+    def post(self, request):
+        stored_posts = request.session.get('stored_posts')
+        post_id = int(request.POST['post_id'])
+
+        if stored_posts is None:
+            stored_posts = []
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+        
+        request.session['stored_posts'] = stored_posts
+
+        return redirect('read_later')
